e674109439b56db8f530ac4d2318a886e12d87a9
Woon Xin Yi <xinyi_woon@hotmail.com>
2019-12-07 16:53:36 +0800

    Add files via upload

---
+++ clics.php
@@ -0,0 +1,10 @@
+/* This is a php folder for clics main page*/
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["nric"]; ?>
+
+</body>
+</html>
+</html>
